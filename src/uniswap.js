676103e6828d70081bc828ac04516432016d3f9c
Souroosh Memarian <sooroushmemarian@gmail.com>
2026-05-03 08:57:40 -0400

    Uniswap.js file version 1

---
+++ src/uniswap.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const url = 'https://trade-api.gateway.uniswap.org/v1/quote';
+
+async function getQuote(tokenIn, tokenOut,amount){
+    try{
+    const response = await axios.post(url, {
+    tokenIn: tokenIn,
+    tokenOut: tokenOut,
+    tokenInChainId: 1301,
+    tokenOutChainId: 1301,
+    type: 'EXACT_INPUT',
+    amount: amount,
+    swapper: process.env.WALLET_ADDRESS,
+    slippageTolerance: 0.5, // 0.5%
+  }, {
+  headers: {
+    'x-api-key': process.env.UNISWAP_API_KEY,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }});
+    console.log(response);
+}
+catch (error) {
+    console.error(error.message);
+}
+}
+  
+module.exports = getQuote;
